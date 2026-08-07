0665a5575d55ef1e754d22fb1a07a536f9e0290e
CorporalCiprian <cipriangheorghe197870@gmail.com>
2026-08-07 11:31:23 +0300

    added env to names of vnet components

--- terraform/vnet.tf
+++ terraform/vnet.tf
@@ -2,7 +2,7 @@
 # Resource Group
 #
 resource "azurerm_resource_group" "rg_vnet" {
-    name = "rg-vnet"
+    name = "rg-vnet-${var.env}"
     location = var.location
 }
 
@@ -10,7 +10,7 @@ resource "azurerm_resource_group" "rg_vnet" {
 # Vnet
 #
 resource "azurerm_virtual_network" "vnet_todo" {
-    name = "vnet-todo"
+    name = "vnet-todo-${var.env}"
     location = azurerm_resource_group.rg_vnet.location
     resource_group_name = azurerm_resource_group.rg_vnet.name
     address_space = ["10.0.0.0/25"]
@@ -20,13 +20,13 @@ resource "azurerm_virtual_network" "vnet_todo" {
 # Subnets
 #
 resource "azurerm_subnet" "snet_backend" {
-    name = "snet-todo-backend"
+    name = "snet-todo-backend-${var.env}"
     resource_group_name = azurerm_resource_group.rg_vnet.name
     virtual_network_name = azurerm_virtual_network.vnet_todo.name
     address_prefixes = [cidrsubnet("10.0.0.0/25",3,0)]
 
     delegation {
-      name = "asp-delegation-backend"
+      name = "asp-delegation-backend-${var.env}"
 
       service_delegation {
         name    = "Microsoft.Web/serverFarms"
@@ -36,12 +36,12 @@ resource "azurerm_subnet" "snet_backend" {
 }
 
 resource "azurerm_subnet" "snet_frontend" {
-    name = "snet-todo-frontend"
+    name = "snet-todo-frontend-${var.env}"
     resource_group_name = azurerm_resource_group.rg_vnet.name
     virtual_network_name = azurerm_virtual_network.vnet_todo.name
     address_prefixes = [cidrsubnet("10.0.0.0/25",3,2)]
     delegation {
-      name = "asp-delegation-frontend"
+      name = "asp-delegation-frontend-${var.env}"
 
       service_delegation {
         name    = "Microsoft.Web/serverFarms"
@@ -52,19 +52,19 @@ resource "azurerm_subnet" "snet_frontend" {
 }
 
 resource "azurerm_subnet" "snet_stg" {
-  name = "snet-todo-stg"
+  name = "snet-todo-stg-${var.env}"
   resource_group_name = azurerm_resource_group.rg_vnet.name
   virtual_network_name = azurerm_virtual_network.vnet_todo.name
   address_prefixes = [cidrsubnet("10.0.0.0/25",3,3)]
 }
 
 resource "azurerm_subnet" "snet_db" {
-    name = "snet-todo-db"
+    name = "snet-todo-db-${var.env}"
     resource_group_name = azurerm_resource_group.rg_vnet.name
     virtual_network_name = azurerm_virtual_network.vnet_todo.name
     address_prefixes = [cidrsubnet("10.0.0.0/25",3,1)]
     delegation {
-      name = "db-delegation"
+      name = "db-delegation-${var.env}"
       service_delegation {
         name    = "Microsoft.DBforPostgreSQL/flexibleServers"
         actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
@@ -80,7 +80,7 @@ resource "azurerm_subnet" "snet_db" {
 # Network Security Groups (NSG)
 #
 resource "azurerm_private_dns_zone_virtual_network_link" "db-dns-link" {
-  name                = "db-dns-link"
+  name                = "db-dns-link-${var.env}"
   resource_group_name = azurerm_resource_group.rg_vnet.name
   private_dns_zone_name = azurerm_private_dns_zone.db_private_dns.name
   virtual_network_id = azurerm_virtual_network.vnet_todo.id
@@ -90,24 +90,24 @@ resource "azurerm_private_dns_zone_virtual_network_link" "db-dns-link" {
 # Private endpoints
 #
 resource "azurerm_private_endpoint" "pep_stg_backend" {
-  name = "pep-stg-backend"
+  name = "pep-stg-backend-${var.env}"
   subnet_id = azurerm_subnet.snet_stg.id
   location = azurerm_resource_group.rg_vnet.location
   resource_group_name = azurerm_resource_group.rg_vnet.name
   private_service_connection {
-    name = "service-conn-backend-stg"
+    name = "service-conn-backend-stg-${var.env}"
     is_manual_connection = false
     private_connection_resource_id = azurerm_storage_account.stg_func_app_bk.id
   }
 }
 
 resource "azurerm_private_endpoint" "pep_stg_frontend" {
-  name = "pep-stg-frontend"
+  name = "pep-stg-frontend-${var.env}"
   subnet_id = azurerm_subnet.snet_stg.id
   location = azurerm_resource_group.rg_vnet.location
   resource_group_name = azurerm_resource_group.rg_vnet.name
   private_service_connection {
-    name = "service-conn-frontend-stg"
+    name = "service-conn-frontend-stg-${var.env}"
     is_manual_connection = false
     private_connection_resource_id = azurerm_storage_account.stg_func_app_fr.id
   }
@@ -130,7 +130,7 @@ resource "azurerm_private_endpoint" "pep_kv" {
 # Network Security Groups (NSG)
 #
 resource "azurerm_network_security_group" "nsg_db" {
-  name = "nsg-db"
+  name = "nsg-db-${var.env}"
   location = azurerm_resource_group.rg_vnet.location
   resource_group_name = azurerm_resource_group.rg_vnet.name
 
