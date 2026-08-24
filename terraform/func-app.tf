5ac306bf4cdfb1ea14523a544e973d62a88db751
CorporalCiprian <cipriangheorghe197870@gmail.com>
2026-08-24 14:58:57 +0300

    implemented func app module calls

--- terraform/func-app.tf
+++ terraform/func-app.tf
@@ -20,143 +20,46 @@ resource "azurerm_service_plan" "asp_func_apps" {
 #
 # Func Apps
 #
-resource "azurerm_linux_function_app" "func_todo_backend" {
-  name                = "func-app-${var.project_name}-backend-${var.env}"
+module "func_app_backend" {
+  source = "git::https://github.com/CorporalCiprian/terraform-modules//modules/func-apps/backend"
   resource_group_name = azurerm_resource_group.rg_todo_func_app.name
-  location            = azurerm_resource_group.rg_todo_func_app.location
-
-  service_plan_id = azurerm_service_plan.asp_func_apps.id
-
-  storage_uses_managed_identity = true
-  storage_account_name          = module.stg_func_app.name
-
-  virtual_network_subnet_id = azurerm_subnet.snet_backend.id
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  site_config {
-    application_stack {
-      python_version = "3.12"
-    }
-    ip_restriction_default_action = "Deny"
-    always_on              = true
-    vnet_route_all_enabled = true
-    ip_restriction {
-      action = "Allow"
-      ip_address = "136.255.102.82/32"
-      priority = 100
-    }
-  }
-
-  app_settings = {
-    "SCM_DO_BUILD_DURING_DEPLOYMENT" = "false"
-    "ENABLE_ORYX_BUILD"              = "false"
-
-    "WEBSITE_VNET_ROUTE_ALL" = "1"
-    "WEBSITE_RUN_FROM_PACKAGE" = "1"
-    "AzureWebJobsFeatureFlags" = "EnableWorkerIndexing"
-    "FUNCTIONS_WORKER_RUNTIME" = "python"
-
-    "DATABASE_URL" = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.connection_string_db.versionless_id})"
-
-    "ALLOWED_ORIGINS"                  = "https://${azurerm_linux_function_app.func_todo_frontend.name}.azurewebsites.net"
-    "AzureWebJobsStorage__accountName" = module.stg_func_app.name
-
-    "env" = var.env
-  }
-
-  lifecycle {
-    ignore_changes = [app_settings["WEBSITE_RUN_FROM_PACKAGE"], app_settings["AzureWebJobsStorage__accountName"], app_settings["WEBSITE_VNET_ROUTE_ALL"],]
-  }
-
-
+  project_name = var.project_name
+  env = var.env
+  location = var.location
+  serviceplan = azurerm_service_plan.asp_func_apps.id
+  stgname = module.stg_func_app.name
+  subnet_id = azurerm_subnet.snet_backend.id
+  networkaccess = "private"
+  allowedorigins = "https://${module.func_app_frontend.name}.azurewebsites.net"
+  build_in_azure = false
+  db_url = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.connection_string_db.versionless_id})"
 }
 
-resource "azurerm_linux_function_app" "func_todo_frontend" {
-  name                = "func-app-${var.project_name}-frontend-${var.env}"
+module "func_app_frontend" {
+  source = "git::https://github.com/CorporalCiprian/terraform-modules//modules/func-apps/frontend"
   resource_group_name = azurerm_resource_group.rg_todo_func_app.name
-  service_plan_id     = azurerm_service_plan.asp_func_apps.id
-  location            = azurerm_resource_group.rg_todo_func_app.location
-
-  storage_uses_managed_identity = true
-  storage_account_name          = module.stg_func_app.name
-
-  virtual_network_subnet_id = azurerm_subnet.snet_frontend.id
-
-  identity {
-    type = "SystemAssigned"
-  }
-  site_config {
-    application_stack {
-      node_version = "24"
-    }
-    always_on = true
-    vnet_route_all_enabled = true
-    ip_restriction_default_action = "Deny"
-
-    ip_restriction {
-      action = "Allow"
-      ip_address = "136.255.102.82/32"
-      priority = 100
-    }
-  }
-
-  app_settings = {
-    "SCM_DO_BUILD_DURING_DEPLOYMENT" = "false"
-    "ENABLE_ORYX_BUILD"              = "false"
-
-    "WEBSITE_RUN_FROM_PACKAGE" = "1"
-    "AzureWebJobsFeatureFlags" = "EnableWorkerIndexing"
-    "FUNCTIONS_WORKER_RUNTIME" = "node"
-
-    "WEBSITE_VNET_ROUTE_ALL" = "1"
-
-    "AzureWebJobsStorage__accountName" = module.stg_func_app.name
-  }
-
-  lifecycle {
-    ignore_changes = [app_settings["WEBSITE_RUN_FROM_PACKAGE"], app_settings["AzureWebJobsStorage__accountName"], app_settings["WEBSITE_VNET_ROUTE_ALL"], ]
-  }
+  project_name = var.project_name
+  env = var.env
+  location = var.location
+  serviceplan = azurerm_service_plan.asp_func_apps.id
+  stgname = module.stg_func_app.name
+  subnet_id = azurerm_subnet.snet_frontend.id
+  build_in_azure = false
+  networkaccess = "private"
 }
 
-resource "azurerm_linux_function_app" "func_app_runner_trigger" {
-  name                = "func-app-${var.project_name}-runner-trigger-${var.env}"
+module "func_app_runner" {
+  source = "git::https://github.com/CorporalCiprian/terraform-modules//modules/func-apps/runner"
   resource_group_name = azurerm_resource_group.rg_todo_func_app.name
-  service_plan_id     = azurerm_service_plan.asp_func_apps.id
-  location            = azurerm_resource_group.rg_todo_func_app.location
-
-  storage_uses_managed_identity = true
-  storage_account_name          = module.stg_func_app.name
-
-  virtual_network_subnet_id = azurerm_subnet.snet_backend.id
-
-  identity {
-    type = "SystemAssigned"
-  }
-  site_config {
-    application_stack {
-      python_version = "3.12"
-    }
-    always_on = true
-    vnet_route_all_enabled = true
-  }
-
-  app_settings = {
-    "AzureWebJobsFeatureFlags" = "EnableWorkerIndexing"
-    "FUNCTIONS_WORKER_RUNTIME" = "python"
-
-    "SCM_DO_BUILD_DURING_DEPLOYMENT" = "true"
-    "Azure_Subscription_Id" = data.azurerm_client_config.current.subscription_id
-    "Vm_Resource_Group" = azurerm_resource_group.rg_vm.name
-    "Vm_Name" = azurerm_linux_virtual_machine.vm_runner.name
-    "AzureWebJobsStorage__accountName" = module.stg_func_app.name
-
-    "env" = var.env
-  }
-
-  lifecycle {
-    ignore_changes = [ app_settings["AzureWebJobsStorage__accountName"], ]
-  }
+  project_name = var.project_name
+  env = var.env
+  location = var.location
+  serviceplan = azurerm_service_plan.asp_func_apps.id
+  stgname = module.stg_func_app.name
+  subnet_id = azurerm_subnet.snet_backend.id
+  networkaccess = "private"
+  subscription_id = data.azurerm_client_config.current.subscription_id
+  vm_rg = azurerm_resource_group.rg_vm.name
+  vm_name = azurerm_linux_virtual_machine.vm_runner.name
+  build_in_azure = true
 }
--- terraform/iam.tf
+++ terraform/iam.tf
@@ -10,13 +10,13 @@ resource "azurerm_role_assignment" "github_principal_kv" {
 data "azurerm_subscription" "current" {}
 
 resource "azurerm_role_assignment" "func_app_role_backend" {
-    principal_id = azurerm_linux_function_app.func_todo_backend.identity[0].principal_id
+    principal_id = module.func_app_backend.principal_id
     scope = module.key_vault.id
     role_definition_name = "Key Vault Secrets Officer"
 }
 
 resource "azurerm_role_assignment" "func_app_role_frontend" {
-    principal_id = azurerm_linux_function_app.func_todo_frontend.identity[0].principal_id
+    principal_id = module.func_app_frontend.principal_id
     scope = module.key_vault.id
     role_definition_name = "Key Vault Secrets Officer"
 }
@@ -28,25 +28,25 @@ resource "azurerm_role_assignment" "github_principal" {
 }
 
 resource "azurerm_role_assignment" "backend_app_storage" {
-    principal_id = azurerm_linux_function_app.func_todo_backend.identity[0].principal_id
+    principal_id = module.func_app_backend.principal_id
     scope = module.stg_func_app.id
     role_definition_name = "Storage Blob Data Owner"
 }
 
 resource "azurerm_role_assignment" "frontend_app_storage" {
-    principal_id = azurerm_linux_function_app.func_todo_frontend.identity[0].principal_id
+    principal_id = module.func_app_frontend.principal_id
     scope = module.stg_func_app.id
     role_definition_name = "Storage Blob Data Owner"
 }
 
 resource "azurerm_role_assignment" "runner_trigger_vm" {
-  principal_id = azurerm_linux_function_app.func_app_runner_trigger.identity[0].principal_id
+  principal_id = module.func_app_runner.principal_id
   scope = azurerm_linux_virtual_machine.vm_runner.id
   role_definition_name = "Virtual Machine Contributor"
 }
 
 resource "azurerm_role_assignment" "runner_trigger_storage" {
-  principal_id = azurerm_linux_function_app.func_app_runner_trigger.identity[0].principal_id
+  principal_id = module.func_app_runner.principal_id
   scope = module.stg_func_app.id
   role_definition_name = "Storage Blob Data Owner"
 }
